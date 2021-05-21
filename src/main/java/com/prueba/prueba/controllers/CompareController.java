2bfb70ea64f0bd024542417dc8a44d460758c3da
alfonso_hagelsieb <alfonsohagelsien@gmail.com>
2021-05-21 13:37:29 -0500

    version 2

--- src/main/java/com/prueba/prueba/controllers/CompareController.java
+++ src/main/java/com/prueba/prueba/controllers/CompareController.java
@@ -3,7 +3,6 @@ package com.prueba.prueba.controllers;
 import com.prueba.prueba.models.CompareModel;
 import com.prueba.prueba.models.ResultModel;
 import org.springframework.web.bind.annotation.*;
-import java.util.*;
 
 @RestController
 public class CompareController {
@@ -11,10 +10,8 @@ public class CompareController {
     @PostMapping("/compare")
     public ResultModel compare(@RequestBody CompareModel compareModel){
         ResultModel r = new ResultModel();
-        List<String> lista1 = compareModel.getReceta1();
-        List<String> lista2 = compareModel.getReceta2();
-        lista1.retainAll(lista2);
-        r.setCoicidentes(lista1);
+        compareModel.getReceta1().retainAll(compareModel.getReceta2());
+        r.setCoicidentes(compareModel.getReceta1());
         return r;
     }
 }
--- src/main/java/com/prueba/prueba/models/CompareModel.java
+++ src/main/java/com/prueba/prueba/models/CompareModel.java
@@ -1,6 +1,5 @@
 package com.prueba.prueba.models;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
